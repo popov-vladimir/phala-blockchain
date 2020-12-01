62402f9739a11ae23d0e1f057dae71aee7404248
vladimir popov <popov.v.aleksandrovich@sberbank.ru>
2020-12-01 20:07:58 +0300

    debug info

--- pruntime/enclave/src/contracts/auction.rs
+++ pruntime/enclave/src/contracts/auction.rs
@@ -4,12 +4,17 @@ use crate::contracts;
 use crate::types::TxRef;
 use crate::TransactionStatus;
 
-/// HelloWorld contract states.
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Auction {
-    bets: BTreeMap<AccountIdWrapper, u32>,
-    winner: Option<AccountIdWrapper>,
-    winning_bet: u32,
+    pub bids: BTreeMap<AccountIdWrapper, u32>,
+    pub winner: Option<AccountIdWrapper>,
+    pub winning_bid: u32,
+}
+
+impl Auction {
+    pub fn new() -> Self {
+        Auction { bids: BTreeMap::new(), winner:None, winning_bid:0 }
+    }
 }
 
 /// The commands that the contract accepts from the blockchain. Also called transactions.
@@ -17,7 +22,7 @@ pub struct Auction {
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Command {
     /// Increments the counter in the contract by some number
-    PlaceBet {
+    PlaceBid {
         value: u32,
     },
 }
@@ -34,17 +39,15 @@ pub enum Error {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Request {
     /// Ask for the value of the counter
-    PlaceBet,
+    PlaceBid {},
 }
 
 /// Query responses.
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Response {
-    /// Returns the value of the counter
     GetWinner {
-        winner: AccountIdWrapper,
+        winner: Option(AccountIdWrapper),
     },
-    /// Something wrong happened
     Error(Error),
 }
 
@@ -52,47 +55,34 @@ pub enum Response {
 impl Auction {
     /// Initializes the contract
     pub fn new() -> Self {
-        // bets: BTreeMap::new(),
-        // winner: None,
-        // winning_bet: 0
-
-        Default::default()
+        Auction { bids: BTreeMap::new(), winner:None, winning_bid:0 }
     }
+
 }
 
 impl contracts::Contract<Command, Request, Response> for Auction {
-    // Returns the contract id
     fn id(&self) -> contracts::ContractId { contracts::AUCTION_HOUSE }
 
-    // Handles the commands from transactions on the blockchain. This method doesn't respond.
     fn handle_command(&mut self, _origin: &chain::AccountId, _txref: &TxRef, cmd: Command) -> TransactionStatus {
         match cmd {
-            // Handle the `Increment` command with one parameter
-            Command::PlaceBet { value } => {
-                // Simply increment the counter by some value.
-
+            Command::PlaceBid { value } => {
                 let current_user = AccountIdWrapper(origin.clone());
-
-                // Insert the note, we only keep the latest note
-                self.bets.insert(current_user, value);
-
-                if value > self.winning_bet {
-                    self.winning_bet = value.clone();
-                    self.winner = current_user;
+                self.bids.insert(current_user, value);
+                println!("got bid {} from {}", value, current_user);
+                if value > self.winning_bid {
+                    println!("setting {} as the new winner", current_user);
+                    self.winning_bid = value.clone();
+                    self.winner = Some(current_user);
                 }
-                // Returns TransactionStatus::Ok to indicate a successful transaction
                 TransactionStatus::Ok
             }
         }
     }
 
-    // Handles a direct query and responds to the query. It shouldn't modify the contract states.
     fn handle_query(&mut self, _origin: Option<&chain::AccountId>, req: Request) -> Response {
         let inner = || -> Result<Response, Error> {
             match req {
-                // Hanlde the `GetCount` request.
                 Request::GetWinner => {
-                    // Respond with the counter in the contract states.
                     Ok(Response::GetWinner { winner: self.winner })
                 }
             }
